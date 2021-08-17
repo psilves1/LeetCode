7d1619bf3604ea1cec786eb2b2c9a3b03711e903
Paul Silvestri <psilves1@binghamton.edu>
2021-08-17 15:21:02 -0400

    Very simple palindrome algorithm

---
+++ palindromeNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        if(x < 0):
+            return False
+        
+        x = str(x)
+        
+        for i in range(int(len(x)/2)):
+            if(x[i] != x[len(x) - i - 1]):
+                return False
+            
+        return True
